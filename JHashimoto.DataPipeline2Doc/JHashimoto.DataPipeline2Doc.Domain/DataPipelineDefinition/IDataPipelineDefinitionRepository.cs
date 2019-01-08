5a506ef1849630dd8903ffd5297e086719628137
jhashimoto <junichi_hashimoto@hotmail.co.jp>
2019-01-08 12:50:53 +0900

    .NET Coreのバージョンを2.1に上げた

---
+++ JHashimoto.DataPipeline2Doc/JHashimoto.DataPipeline2Doc.Domain/DataPipelineDefinition/IDataPipelineDefinitionRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JHashimoto.DataPipeline2Doc.Domain.DataPipelineDefinition {
+    public interface IDataPipelineDefinitionRepository {
+        string FindById(string key);
+    }
+}
